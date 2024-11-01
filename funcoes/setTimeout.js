204573e0b6ff4f8dce06e755b2e537d21987dbd4
yas808 <yasmincristineferreira21@gmail.com>
2024-11-01 18:20:20 +0000

    Clear Interval e Setinterval no JavaScript

--- funcoes/setTimeout.js
+++ funcoes/setTimeout.js
@@ -2,3 +2,18 @@
 setTimeout(function(){
     console.log("Tempo esgotado")
 }, 5000)
+
+
+// setInterval (função, tempo ms) //
+let fps = Math.round( Math.random() * 30)
+
+const intervalo1 = setInterval(function (){
+    console.clear() // LIMPAR A TELA //
+    console.log('Fps: ' + fps)
+    fps = fps + 1
+}, 1000)
+
+setTimeout(function() {
+    clearInterval(intervalo1)
+    console.log('Conexão encerrada!')
+}, 5000)
